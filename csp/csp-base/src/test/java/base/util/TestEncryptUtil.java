125e2c6f5cdd94477a088107bf673788ac0ebbe0
Mark Wong <yefang.wong@gmail.com>
2015-11-22 23:20:07 +0800

    專案加密工具。

---
+++ csp/csp-base/src/test/java/base/util/TestEncryptUtil.java
@@ -0,0 +1,21 @@
+package base.util;
+import java.text.MessageFormat;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+
+import base.util.BaseLogger;
+import base.util.EncryptUtil;
+public class TestEncryptUtil {
+	private static Logger logger = new BaseLogger(TestEncryptUtil.class);
+
+	@Test
+	public void test() {
+		String cardNum = "1234567890111";
+		//test encrypt
+		logger.info(MessageFormat.format("card num encode result {0}", EncryptUtil.encrypt(cardNum)));
+		
+		//test decrypt
+		logger.info(MessageFormat.format("card num decode result {0}", EncryptUtil.decrypt("41JvM5-x3tQaZl9ZpU7LpQ")));
+	}
+}
