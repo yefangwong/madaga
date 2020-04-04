cc4a4f7bf4647a91f9e9f92ad1f5040f684399ee
yefang wong <yefang.wong@gmail.com>
2020-04-04 13:52:13 +0800

    新增 Portal 專案。

---
+++ csp/csp-portal-web/src/main/java/com/hongfang/csp/portal/CspPortalWebApplication.java
@@ -0,0 +1,13 @@
+package com.hongfang.csp.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CspPortalWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CspPortalWebApplication.class, args);
+	}
+
+}
---
+++ csp/csp-portal-web/src/test/java/com/hongfang/csp/portal/CspPortalWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hongfang.csp.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CspPortalWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
