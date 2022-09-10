c2ec9b1ab15bf348a4646bc8f2a67a9c1ceb2caf
yefangwong <yefang.wong@gmail.com>
2022-09-10 11:42:00 +0800

    部署 API 到 GCP

--- csp/csp-api/src/main/java/com/hongfang/csp/api/controller/PasswdController.java
+++ csp/csp-api/src/main/java/com/hongfang/csp/api/controller/PasswdController.java
@@ -35,8 +35,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.text.MessageFormat;
-
 @RequestMapping("/v1/passwd")
 @RestController
 public class PasswdController {
@@ -44,10 +42,10 @@ public class PasswdController {
 
     @PostMapping(value = "/decode/{str}", produces="application/json")
     public String decode(@PathVariable Object str) throws Exception {
-        logger.info("PasswdController decode");
+        //logger.info("PasswdController decode");
         Object result = EncryptUtil.decrypt(str);
         //decrypt
-        logger.info(MessageFormat.format("string decode result {0}", result));
+        //logger.info(MessageFormat.format("string decode result {0}", result));
         return (String)result;
     }
 }
