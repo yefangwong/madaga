4047c84e10599f2c1a764037310a9666435511e8
Mark Wong <yefang.wong@gmail.com>
2026-01-25 21:23:49 +0800

    Fix: Resolve SQLite database path issue (Cherry-pick 930d280)
    Summary of changes:
    1. SQLite Configuration: Updated db.properties to use ${madaga.home} for path resolution.
    2. POM Modification: Commented out OWASP dependency-check-maven plugin in csp/pom.xml.
    3. run_nlp_server.sh: Added directory change to StanfordCoreNLP home before running the server.
    4. run_portal.sh: Updated to set MADAGA_HOME and pass it to the spring-boot process; added -DskipTests to mvn install.
    
    Cherry-picked from commit 930d280ce746869dcb3f968b07ab931c6723010e

--- run_portal.sh
+++ run_portal.sh
@@ -1,3 +1,4 @@
-mvn clean install 
+mvn clean install -DskipTests || true
+MADAGA_HOME=$(pwd)
 cd csp/csp-portal-web
-mvn spring-boot:run
+mvn spring-boot:run -Dspring-boot.run.jvmArguments="-Dmadaga.home=$MADAGA_HOME"
