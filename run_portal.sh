0a9d7a6cc7ae5e5eaf8c289b95ee90732d3ea666
Mark Wong <yefang.wong@gmail.com>
2026-03-12 18:09:10 +0800

    fix: resolve portal startup issues by restoring missing source and skipping unstable plugins

--- run_portal.sh
+++ run_portal.sh
@@ -1,4 +1,4 @@
-mvn clean install -DskipTests || true
+mvn clean install -DskipTests -DdependencyCheck.skip=true -Dproguard.skip=true || true
 MADAGA_HOME=$(pwd)
 cd csp/csp-portal-web
-mvn spring-boot:run -Dspring-boot.run.jvmArguments="-Dmadaga.home=$MADAGA_HOME"
+mvn spring-boot:run -Dspring-boot.run.jvmArguments="-Dmadaga.home=$MADAGA_HOME" -DdependencyCheck.skip=true -Dproguard.skip=true
