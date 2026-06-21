56ffe968160982bf33531037b164d35b59cead04
Mark Wong <yefang.wong@gmail.com>
2026-06-21 18:10:11 +0800

    加入聯絡我們

--- csp/csp-portal-web/src/main/java/com/hongfang/csp/portal/controller/ContactController.java
+++ csp/csp-portal-web/src/main/java/com/hongfang/csp/portal/controller/ContactController.java
@@ -22,7 +22,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
